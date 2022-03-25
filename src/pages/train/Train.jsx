57cd622a217d6da0e956200a94b2393e83d2d60f
sadmad <sarmad.re@gmail.com>
2022-03-25 11:32:19 +0100

    working on the form

--- src/pages/train/Train.jsx
+++ src/pages/train/Train.jsx
@@ -1,5 +1,13 @@
+import TrainForm from "./TrainForm";
+import './Train.css'
+
 const Train = () => {
 
+    return(
+        <div className="train">
+            <TrainForm />
+        </div>
+    )
 }
 
 export default Train;   
---
+++ src/pages/train/TrainForm.jsx
@@ -0,0 +1,68 @@
+import "./TrainForm.css";
+
+const TrainForm = () => {
+  console.log("hello");
+
+  return (
+    <form>
+      <div>
+          <h3>Please select the model on which you want to train the data</h3>
+        <div>
+          <input type="radio" id="NN" name="drone" value="1" />
+          <label for="huey">Neural Network(scikit learn)</label>
+        </div>
+        <div>
+          <input type="radio" id="RF" name="drone" value="2" />
+          <label for="huey">RANDOM FOREST(scikit learn)</label>
+        </div>
+        <div>
+          <input type="radio" id="LR" name="drone" value="3" />
+          <label for="huey">LINEAR REGRESSION(scikit learn)</label>
+        </div>
+        <div>
+          <input type="radio" id="NN" name="drone" value="5" />
+          <label for="huey">DEEP NEURAL NETWORK(Keras+tensorflow)</label>
+        </div>
+        <div>
+          <input type="radio" id="NN" name="drone" value="6" />
+          <label for="huey">Decision Tree(scikit learn)</label>
+        </div>
+      </div>
+      <div>
+          <h3>Choose The Amucad field(action)</h3>
+        <div>
+          <input type="radio" id="NN" name="drone" value="1" />
+          <label for="huey">Explosion</label>
+        </div>
+        <div>
+          <input type="radio" id="RF" name="drone" value="2" />
+          <label for="huey">Corrosion</label>
+        </div>
+      </div>
+      <div>
+        <div>
+          <input type="radio" id="NN" name="drone" value="1" />
+          <label for="huey">Explosion</label>
+        </div>
+        <div>
+          <input type="radio" id="RF" name="drone" value="2" />
+          <label for="huey">Corrosion</label>
+        </div>
+        <div>
+          <input type="radio" id="RF" name="drone" value="3" />
+          <label for="huey">Corrosion</label>
+        </div>
+        <div>
+          <input type="radio" id="RF" name="drone" value="4" />
+          <label for="huey">Corrosion</label>
+        </div>
+        <div>
+          <input type="radio" id="RF" name="drone" value="5" />
+          <label for="huey">Corrosion</label>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default TrainForm;
